8d332fbd7fea6cfa18a8e010f9941adeb0c689a3
Reece Clem <reececlem@Reeces-MacBook-Pro.local>
2026-05-12 23:58:57 -0400

    Removing old folder

--- KBK - Picability/KBK - Picability/DTOs/CreateFriendRequestDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Picability.DTOs
-{
-    public class CreateFriendRequestDto
-    {
-        public string SenderId { get; set; } = string.Empty;
-        public string ReceiverId { get; set; } = string.Empty;
-    }
-}
--- KBK - Picability/KBK - Picability/DTOs/CreateStreakRequestDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Picability.DTOs
-{
-    public class CreateStreakRequestDto
-    {
-        public string SenderId { get; set; } = string.Empty;
-        public string ReceiverId { get; set; } = string.Empty;
-        public string HabitName { get; set; } = string.Empty;
-    }
-}
